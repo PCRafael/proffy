209997c35f6e5dd1ceaf1cdff3b6c9fe9d750eb4
Rafael Couto <rafael6291@gmail.com>
2020-08-06 23:42:22 -0300

    Alguns arquivos que ficaram pra trás no commit sabe lá porque

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,58 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number,
+  subject: string,
+  cost: number,
+  name: string,
+  avatar: string,
+  whatsapp: string,
+  bio: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection(){
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars1.githubusercontent.com/u/54128315?s=460&u=cb00e9c8404f164a63dfc8217195071a02a192cd&v=4" alt="Rafael Couto"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Rafael Couto</strong>
-        <span>Tênis de mesa</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
     <p>
-      Entusiasta das mais avançadas técnicas de tênis de mesa.
-      <br/><br/>
-      Apaixonado em ensinar pessoas a sairem do ping-pong e se dedicarem mais a este esporte maravilhoso.
+    {teacher.bio}
     </p>
 
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 80,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a 
+        target="_blank"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}
+      >
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   );
